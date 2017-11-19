e688275c08b0bb1592abd1ba9a7e647970c65674
Dev-Laurin <lafisher@alaska.edu>
2017-11-18 16:06:39 -0900

    Labels on graph completed.

--- iOS/fbxpollenfallen/fbxpollenfallen/ViewController.swift
+++ iOS/fbxpollenfallen/fbxpollenfallen/ViewController.swift
@@ -11,9 +11,7 @@ import Charts
 
 class ViewController: UIViewController, ChartViewDelegate {
 
-
     @IBOutlet weak var yAxisView: UIView!
-    @IBOutlet weak var yAxisLabel: UILabel!
     @IBOutlet weak var lineChartVerticalStackView: UIStackView!
     @IBOutlet weak var GraphLabel: UILabel!
     @IBOutlet weak var xAxisLabel: UILabel!
@@ -22,9 +20,13 @@ class ViewController: UIViewController, ChartViewDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        yAxisLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
-       // yAxisLabel.bounds.width = yAxisView.bounds.height
-        //yAxisView.frame.size.width = 10
+        let screen = UIScreen.main.bounds
+        
+        var label = UILabel(frame: CGRect(x: yAxisView.bounds.width/4, y: screen.size.height/2, width: 100, height: 100))
+        label.text = "Pollen Intensity"
+        label.transform = CGAffineTransform(rotationAngle: -CGFloat.pi/2)
+        label.frame = CGRect(x: yAxisView.bounds.width/8, y: 0, width: 20, height: yAxisView.bounds.height/2)
+        yAxisView.addSubview(label)
         
         self.lineChart.delegate = self
         self.lineChart.chartDescription?.text = "Tap node for details"
