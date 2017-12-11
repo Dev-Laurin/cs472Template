8bb298ac7d679818513a699ff1c7b8e234c47531
Jacob McKenna <mcjacob61@gmail.com>
2017-12-11 13:14:29 -0900

    fix

--- server/backend_server.py
+++ server/backend_server.py
@@ -13,180 +13,171 @@ from collections import deque
 class echoServer(BaseHTTPRequestHandler):
 
 
-    # Create DB connections, return cursor.
-    def DBconnection(self):
-        self.conn = pymysql.connect(
-            host="127.0.0.1",
-            port='3306',
-            user="sneezy",
-            passwd="sneezing",
-            db="pollen",
-            unix_socket="/var/run/mysqld/mysqld.sock"
-            )
+	# Create DB connections, return cursor.
+	def DBconnection(self):
+		self.conn = pymysql.connect(
+			host="127.0.0.1",
+			port='3306',
+			user="sneezy",
+			passwd="sneezing",
+			db="pollen",
+			unix_socket="/var/run/mysqld/mysqld.sock"
+			)
 
-        return self.conn.cursor()
+		return self.conn.cursor()
 
-    # Close DB connection.
-    def closeDBconnection(self):
-        self.conn.close()
+	# Close DB connection.
+	def closeDBconnection(self):
+		self.conn.close()
 
-    # Pollen Query
-    # Adjust query as needed. You'll need to update jsonify() below
-    # to capture the correct items you're looking for. 
-    def pollenQuery(self):
-        query = "SELECT date, alder, willow, \
-            poplar_aspen, birch, spruce, \
-            other1_tree, other2_tree, grass, grass_2, \
-            weed, other1, other2, mold FROM PollenData;"
+	# Pollen Query
+	# Adjust query as needed. You'll need to update jsonify() below
+	# to capture the correct items you're looking for. 
+	def pollenQuery(self):
+		query = "SELECT date, alder, willow, \
+			poplar_aspen, birch, spruce, \
+			other1_tree, other2_tree, grass, grass_2, \
+			weed, other1, other2, mold FROM PollenData;"
 
-        return query
+		return query
 
-    def tableQuery(self):
-        query = "desc PollenData;"
+	def tableQuery(self):
+		query = "desc PollenData;"
 
-        return query
+		return query
 
-    # On GET request, query the MySQL DB, format data into pollendata blocks, and return
-    # as a string.
-
-
-    # Regular jsonify function. Returns regular JSON data, non-separated. 
-    def jsonify(self, response):
-        
-        allPollenData = {}
-
-        polArray = []
-
-        counter = 0
-        for r in response:
-
-            emptyDict = {
-                "Year": r[0].year,
-                "Data":{
-
-                    "Day": r[0].day,
-                    "Month": r[0].month,
-                    
-                    "Alder": r[1], 
-                    "Willow": r[2], 
-                    "Poplar Aspen": r[3], 
-                    "Birch":r[4],
-                    "Spruce": r[5], 
-                    "Other1 Tree": r[6], 
-                    "Other2 Tree": r[7], 
-                    "Grass": r[8], 
-                    "Grass2":r[9],
-                    "Weed": r[10], 
-                    "Other1": r[11], 
-                    "Other2": r[12],
-                    "Mold": r[13]
-                },
-            }
-
-            polArray.append(emptyDict)
-
-        self.easyJsonify(response)
-
-        return polArray
+	# On GET request, query the MySQL DB, format data into pollendata blocks, and return
+	# as a string.
 
 
-    # Easy to parse JSON, categorized by year objects. 
-    def easyJsonify(self, response):
+	# Regular jsonify function. Returns regular JSON data, non-separated. 
+	def jsonify(self, response):
+		
+		allPollenData = {}
 
-        # Store to JSON array.
-        polArray = []
+		polArray = []
 
-        yearHolder = response[0][0].year # Set starting year.
+		counter = 0
+		for r in response:
 
-        print(yearHolder, "starting year") 
+			emptyDict = {
+				"Year": r[0].year,
+				"Data":{
 
-        workingYear = {
-            "Year": 0,
-            "Data": []
-        }
+					"Day": r[0].day,
+					"Month": r[0].month,
+					
+					"Alder": r[1], 
+					"Willow": r[2], 
+					"Poplar Aspen": r[3], 
+					"Birch":r[4],
+					"Spruce": r[5], 
+					"Other1 Tree": r[6], 
+					"Other2 Tree": r[7], 
+					"Grass": r[8], 
+					"Grass2":r[9],
+					"Weed": r[10], 
+					"Other1": r[11], 
+					"Other2": r[12],
+					"Mold": r[13]
+				},
+			}
+
+			polArray.append(emptyDict)
+
+		return polArray
 
 
-        lists = []
+	# Easy to parse JSON, categorized by year objects. 
+	def easyJsonify(self, response):
 
-        for r in response:
-            
-            # Adjust for when year changes. 
-            if (yearHolder != r[0].year):
-                print(r[0].year, yearHolder)
+		# Store to JSON array.
+		polArray = []
 
-                # Assign and save/copy. 
-                workingYear["Year"] = yearHolder
-                workingYear["Data"] = lists
-                polArray.append(deepcopy(workingYear))
+		yearHolder = response[0][0].year # Set starting year.
 
-                # Switch to previous year.
-                yearHolder = r[0].year
-                # Clean list. 
-                lists = []
-                # workingYear["Year"] = r[0].year
+		print(yearHolder, "starting year") 
+
+		workingYear = {
+			"Year": 0,
+			"Data": []
+		}
+
+
+		lists = []
+
+		for r in response:
+			
+			# Adjust for when year changes. 
+			if (yearHolder != r[0].year):
+
+				# Assign and save/copy. 
+				workingYear["Year"] = yearHolder
+				workingYear["Data"] = lists
+				polArray.append(deepcopy(workingYear))
+
+				# Switch to previous year.
+				yearHolder = r[0].year
+				# Clean list. 
+				lists = []
+				# workingYear["Year"] = r[0].year
 
 
 
-            lists.append({
+			lists.append({
 
-                "Day": r[0].day,
-                "Month": r[0].month,
-                "Alder": r[1], 
-                "Willow": r[2], 
-                "Poplar Aspen": r[3], 
-                "Birch":r[4],
-                "Spruce": r[5], 
-                "Other1 Tree": r[6], 
-                "Other2 Tree": r[7], 
-                "Grass": r[8], 
-                "Grass2":r[9],
-                "Weed": r[10], 
-                "Other1": r[11], 
-                "Other2": r[12],
-                "Mold": r[13]
+				"Day": r[0].day,
+				"Month": r[0].month,
+				"Alder": r[1], 
+				"Willow": r[2], 
+				"Poplar Aspen": r[3], 
+				"Birch":r[4],
+				"Spruce": r[5], 
+				"Other1 Tree": r[6], 
+				"Other2 Tree": r[7], 
+				"Grass": r[8], 
+				"Grass2":r[9],
+				"Weed": r[10], 
+				"Other1": r[11], 
+				"Other2": r[12],
+				"Mold": r[13]
 
-            })
-
-
+			})
 
 
-
-            counter+=1
-                # allYears[str(yearHolder)] = (workingYear)
-
-        workingYear["Year"] = yearHolder
-        workingYear["Data"] = lists
-        polArray.append(deepcopy(workingYear))
-        print(counter)
-        
-        return json.dumps(polArray)
+		# Needed, for now, to catch final year. 
+		workingYear["Year"] = yearHolder
+		workingYear["Data"] = lists
+		polArray.append(deepcopy(workingYear))
+		
+		return json.dumps(polArray)
 
 
-    def do_GET(self):
+	def do_GET(self):
 
-        # Use this for passing parameters. Functions for specific query still need to 
-        # dealt with. 
-        params = parse_qs(urlparse(self.path).query)
+		# Use this for passing parameters. Functions for specific query still need to 
+		# dealt with. 
+		params = parse_qs(urlparse(self.path).query)
 
 
-        cursor = self.DBconnection()
-        cursor.execute(self.pollenQuery())
-        
-        # Retrieve ALL data.
-        response = cursor.fetchall()
+		cursor = self.DBconnection()
+		cursor.execute(self.pollenQuery())
+		
+		# Retrieve ALL data.
+		response = cursor.fetchall()
 
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(bytes(str(self.easyJsonify(response)),"utf8"))
-        # self.wfile.write(bytes(self.easyJsonify(response)))
+		self.send_response(200)
+		self.end_headers()
+		self.wfile.write(bytes(str(self.easyJsonify(response)),"utf8"))
+		# self.wfile.write(bytes(self.easyJsonify(response)))
 
-        self.closeDBconnection()
+		self.closeDBconnection()
 
 
 
 
 
 if __name__ == '__main__':
-    server_address = ('127.0.0.1',8080)
-    server = HTTPServer(server_address,echoServer)
-    server.serve_forever()
+	server_address = ('127.0.0.1',8080)
+	server = HTTPServer(server_address,echoServer)
+	server.serve_forever()
