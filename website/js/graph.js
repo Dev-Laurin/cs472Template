e9642c0e3218b252cdcff87b64d9c045c135b928
amitchell <amitchell@alaska.edu>
2017-12-12 20:03:39 -0900

    added color keys to checkboxes, year slider

--- website/js/graph.js
+++ website/js/graph.js
@@ -1,77 +1,78 @@
-var svg = d3.select("svg"),
-    margin = {top: 20, right: 80, bottom: 30, left: 50},
-    width = svg.attr("width") - margin.left - margin.right,
-    height = svg.attr("height") - margin.top - margin.bottom,
-    g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var pollen_data;
 
-var parseTime = d3.timeParse("%Y%m%d");
+var selector = document.getElementById("alder");
 
-var x = d3.scaleTime().range([0, width]),
-    y = d3.scaleLinear().range([height, 0]),
-    z = d3.scaleOrdinal(d3.schemeCategory10);
+function update_pollen()
+{
+  plot_data(selector.value);
+}
+selector.addEventListener('change',update_pollen,false);
 
-var line = d3.line()
-.curve(d3.curveBasis)
-.x(function(d) { return x(d.date); })
-.y(function(d) { return y(d.temperature); });
+Plotly.d3.json(encodeURI("https://fbxpollenfallen.com/pollen/"), function(data)
+{
+  pollen_data = data;
+  console.log(data);
+  var rawDataURL = 'https://raw.githubusercontent.com/plotly/datasets/master/2016-weather-data-seattle.csv';/*'https://fbxpollenfallen.com/pollen/';*/
+  var xField = 'Date';
+  var yField = 'Mean_TemperatureC';
 
-d3.tsv("data.tsv", type, function(error, data) {
-  if (error) throw error;
+var selectorOptions = {
+  buttons: [{
+    step: 'month',
+    stepmode: 'backward',
+    count: 1,
+    label: '1m'
+  }, {
+    step: 'month',
+    stepmode: 'backward',
+    count: 6,
+    label: '6m'
+  }, {
+    step: 'year',
+    stepmode: 'backward',
+    count: 1,
+    label: '1y'
+  }, {
+    step: 'year',
+    stepmode: 'backward',
+    count: 3,
+    label: '3y'
+  }, {
+    step: 'all',
+  }],
+};
 
-  var cities = data.columns.slice(1).map(function(id) {
-    return {
-      id: id,
-      values: data.map(function(d) {
-        return {date: d.date, temperature: d[id]};
-      })
-    };
-  });
+Plotly.d3.csv(rawDataURL, function(err, rawData) {
+  if(err) throw err;
 
-  x.domain(d3.extent(data, function(d) { return d.date; }));
+  var data = prepData(rawData);
+  var layout = {
+    xaxis: {
+      rangeselector: selectorOptions,
+      rangeslider: {}
+    },
+    yaxis: {
+      fixedrange: true
+    }
+  };
 
-  y.domain([
-    d3.min(cities, function(c) { return d3.min(c.values, function(d) { return d.temperature; }); }),
-    d3.max(cities, function(c) { return d3.max(c.values, function(d) { return d.temperature; }); })
-  ]);
-
-  z.domain(cities.map(function(c) { return c.id; }));
-
-  g.append("g")
-    .attr("class", "axis axis--x")
-    .attr("transform", "translate(0," + height + ")")
-    .call(d3.axisBottom(x));
-
-  g.append("g")
-    .attr("class", "axis axis--y")
-    .call(d3.axisLeft(y))
-    .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", "0.71em")
-    .attr("fill", "#000")
-    .text("Temperature, ºF");
-
-  var city = g.selectAll(".city")
-  .data(cities)
-  .enter().append("g")
-  .attr("class", "city");
-
-  city.append("path")
-    .attr("class", "line")
-    .attr("d", function(d) { return line(d.values); })
-    .style("stroke", function(d) { return z(d.id); });
-
-  city.append("text")
-    .datum(function(d) { return {id: d.id, value: d.values[d.values.length - 1]}; })
-    .attr("transform", function(d) { return "translate(" + x(d.value.date) + "," + y(d.value.temperature) + ")"; })
-    .attr("x", 3)
-    .attr("dy", "0.35em")
-    .style("font", "10px sans-serif")
-    .text(function(d) { return d.id; });
+  Plotly.plot('graph', data, layout);
 });
 
-function type(d, _, columns) {
-  d.date = parseTime(d.date);
-  for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
-  return d;
+function prepData(rawData) {
+  var x = [];
+  var y = [];
+
+  rawData.forEach(function(datum, i) {
+
+    x.push(new Date(datum[xField]));
+    y.push(datum[yField]);
+  });
+
+  return [{
+    mode: 'lines',
+    x: x,
+    y: y
+  }];
 }
+});
